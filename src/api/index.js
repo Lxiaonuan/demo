67703ffae34d8341dd0eb2e4b49b4837595139c5
liguoxiang <12345678>
2021-12-16 13:51:15 +0800

    ceshiluoji

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import request from '../utils/request'
-// import qs from 'qs';  //用于格式化 json
+// import qs from 'qs';  //用于格式化 json 很好用
 export function getdata(params){
    return request({
     url: '/WmsBigTv/GetTempData',
