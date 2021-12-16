ac63aee5d40a04c8acc8b84eb29b6699ede5d4af
liguoxiang <12345678>
2021-12-16 13:58:37 +0800

    测试2

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import request from '../utils/request'
-// import qs from 'qs';  //用于格式化 json 很好用
+// import qs from 'qs';  //用于格式化 json 很好用啊啊啊
 export function getdata(params){
    return request({
     url: '/WmsBigTv/GetTempData',
